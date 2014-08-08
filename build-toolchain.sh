a428cc060499081207b998772914b2d4f9406272
rpedde <ron@pedde.com>
2014-08-08 14:05:38 -0500

    use zerovm branch of gcc

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -80,10 +80,12 @@ git_refresh https://github.com/zerovm/zerovm devel
 git_refresh https://github.com/zerovm/validator master
 git_refresh https://github.com/zerovm/zrt master ${TOOLCHAIN_PATH}/zrt
 git_refresh https://github.com/zerovm/toolchain master ${TOOLCHAIN_PATH}/toolchain
+git_refresh https://github.com/zerovm/gcc zerovm ${TOOLCHAIN_PATH}/SRC/gcc
 
-for d in linux-headers-for-nacl gcc glibc newlib binutils; do
-    git_refresh https://github.com/zerovm/${d} master toolchain/SRC/${d}
+for d in linux-headers-for-nacl glibc newlib binutils; do
+    git_refresh https://github.com/zerovm/${d} master ${TOOLCHAIN_PATH}/SRC/${d}
 done
+
 
 pushd validator
 do_configure
